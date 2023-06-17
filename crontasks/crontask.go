ffcf10703f3805856b73fc383a247be9c1081192
RomanOvc <roman.ovcharov.97@mail.ru>
2023-06-17 13:59:40 +0300

    crontask re

--- crontasks/crontask.go
+++ crontasks/crontask.go
@@ -11,14 +11,14 @@ import (
 func RunCronJob(db *sql.DB) {
 	s := gocron.NewScheduler(time.UTC)
 	ctx := context.TODO()
-	// s.Cron("0 1 * * *").Do(func() {
-	// 	AddCountVisitOnIURLPerDay(db, ctx)
-	// })
+	s.Cron("0 1 * * *").Do(func() {
+		AddCountVisitOnURLPerDay(db, ctx)
+	})
 
 	// for test
-	s.Cron("* * * * *").Do(func() {
-		AddCountVisitOnIURLPerDay(db, ctx)
-	})
+	// s.Cron("* * * * *").Do(func() {
+	// 	AddCountVisitOnIURLPerDay(db, ctx)
+	// })
 	s.StartBlocking()
 
 }
--- crontasks/postgres_inuirys.go
+++ crontasks/postgres_inuirys.go
@@ -9,21 +9,18 @@ import (
 	"golang.org/x/net/context"
 )
 
-type VisitStatistic struct {
+type visitStatistic struct {
 	Shorturl          string `json:"shorturl"`
 	CountUniqueVisits int    `json:"count_unique_visits"`
 	CountAllVisits    int    `json:"count_all_visits"`
 	ShorturlId        int    `json:"shorturl_id"`
 }
 
-func AddCountVisitOnIURLPerDay(db *sql.DB, ctx context.Context) {
+func AddCountVisitOnURLPerDay(db *sql.DB, ctx context.Context) {
 	dateYesterday := time.Now().Add(-24 * time.Hour)
 	dateStrat := fmt.Sprint(dateYesterday.Format("2006-01-02"))
-
 	dateStop := fmt.Sprint(time.Now().Format("2006-01-02"))
 
-	log.Println(dateStrat)
-	log.Println(dateStop)
 	// добавить порсент или долю  уникальынх визитов сколько% визитов было уникальных от общего числа посещения
 	rows, err := db.QueryContext(ctx, `SELECT count(distinct useragent) as count_unique_visits, count(useragent) as count_vists,  shorturl, shorturl_id FROM activity WHERE click_time BETWEEN $1 AND $2 GROUP BY shorturl,shorturl_id ORDER BY shorturl ASC;`, dateStop, dateStrat)
 	if err != nil {
@@ -31,9 +28,10 @@ func AddCountVisitOnIURLPerDay(db *sql.DB, ctx context.Context) {
 	}
 	defer rows.Close()
 
-	var visitOnUrl []VisitStatistic
+	var visitOnUrl []visitStatistic
+
 	for rows.Next() {
-		var v VisitStatistic
+		var v visitStatistic
 		err := rows.Scan(&v.CountUniqueVisits, &v.CountAllVisits, &v.Shorturl, &v.ShorturlId)
 		if err != nil {
 			log.Fatal(err)
@@ -41,20 +39,17 @@ func AddCountVisitOnIURLPerDay(db *sql.DB, ctx context.Context) {
 
 		visitOnUrl = append(visitOnUrl, v)
 	}
+
 	if err = rows.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(visitOnUrl)
-	// unnest (вставка несколикх объектов)
-	// подход к select изменить
 	for _, g := range visitOnUrl {
 		res, err := db.ExecContext(ctx, "INSERT INTO visit_statistic(count_unique_visit, count_all_visit , index_url ,shorturl_id, date) values ($1,$2,$3,$4,$5)", g.CountUniqueVisits, g.CountAllVisits, g.Shorturl, g.ShorturlId, time.Now().Format("2006-01-02 15:04:05"))
 		if err != nil {
 			log.Println(g)
 			log.Fatal(err)
 		}
-		//  сколько получилось с res сравнивать с количестов, которое я добавил. Если неравенство, записать в логах ошибку
 		res.RowsAffected()
 	}
 }
